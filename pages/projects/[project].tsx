1a6217600888fbb7542894c2bf57f97a7ddfd992
Niall Sharma <Niall.k.sharma@gmail.com>
2022-02-06 16:08:18 -0600

    Hope

--- pages/projects/[project].tsx
+++ pages/projects/[project].tsx
@@ -4,9 +4,17 @@ import Layout from "../../wrappers/Layout";
 import Link from "next/link";
 import NavBar from "../../components/Navbar";
 import projects from '../../util/projectViews.json'
+
+export async function getServerSideProps(context) {
+    return {
+      props: {}, // will be passed to the page component as props
+    }
+  }
+
 const Project = () => {
     const router = useRouter();
-    const {project} = router.query
+    const {project} = projects[router.query];
+    if(!project) return <p></p>;
     return(
         <>
             <NavBar />
