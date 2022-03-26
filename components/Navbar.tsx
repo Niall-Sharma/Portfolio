396e9fb92bd199f6221713be89d4aea5c168f8e3
Niall Sharma <Niall.k.sharma@gmail.com>
2022-03-25 20:04:02 -0500

    Sticky Nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,7 @@ import NavButton from './navButton'
 const NavBar = () => {
     return(
     <>
-        <nav className="relative flex flex-wrap items-center justify-between px-1 py-3  bg-white drop-shadow-xl">
+        <nav className="flex flex-wrap items-center justify-between px-1 py-3  bg-white drop-shadow-xl sticky top-0 z-50">
             <div className="mr-auto flex flex-wrap items-center">
                     <ul className="flex flex-col lg:flex-row list-none">
                         <NavButton navLink='/'>
--- pages/contactPage.tsx
+++ pages/contactPage.tsx
@@ -8,7 +8,10 @@ const contactPage = () => {
         <>
             <NavBar />
             <Layout title="Contact">
-
+                <div className="mx-auto container w-5/6">
+                    <h1 className="text-7xl mt-5">Contact</h1>
+                    <p className="mt-5 mx-2 text-xl">Email: Niall.k.Sharma@gmail.com</p>
+                </div>
             </Layout>
         </>
     )
